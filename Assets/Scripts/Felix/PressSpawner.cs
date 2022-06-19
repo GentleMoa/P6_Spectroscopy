8b3b783bb447dd90b86d3f43ebdbb35577d89271
artembrodetskiy <artembroad@gmail.com>
2022-06-19 22:57:20 +0200

    New Lab (Beta Version)
    lab model with textures (Also kinematic of the panels was changed)

--- Assets/Scripts/Felix/PressSpawner.cs
+++ Assets/Scripts/Felix/PressSpawner.cs
@@ -10,9 +10,9 @@ public class PressSpawner : MonoBehaviour
     void Start()
     {
         PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "PressParent"), new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Blue_Panel"), new Vector3(1.0f, 1.8f, 15.9f), Quaternion.identity);
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Purple_Panel"), new Vector3(1.1f, 1.81f, 17.7f), Quaternion.identity);
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Orange_Panel"), new Vector3(1.2f, 1.8f, 16.8f), Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Blue_Panel"), new Vector3(1.0f, 1.92f, 14f), Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Purple_Panel"), new Vector3(1.1f, 1.92f, 15f), Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPanels", "Orange_Panel"), new Vector3(1.2f, 1.92f, 16f), Quaternion.identity);
     }
 
 }
